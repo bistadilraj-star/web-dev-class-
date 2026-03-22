ac02cd61c42383d1ca833e08595f3f1cb3330c95
bistadilraj-star <bist.dilraj@email.com>
2026-03-22 10:12:17 +0545

    Added js class 3 project

---
+++ js-class-2/script.js
@@ -0,0 +1,9 @@
+let a = 30;
+if (a>20){
+    alert("a is greater than 20" +  ". The value of a is: " + a);
+}else if (a==20){
+    alert(`a is equal to 20. The value of a is: ${a}`);
+}else
+{
+    alert("a is not greater than 20" + ". The value of a is: " + a);
+}
